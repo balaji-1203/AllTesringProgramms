359a166d050fb32960513cf722273f9226436670
balaji-1203 <67429470+balaji-1203@users.noreply.github.com>
2022-06-15 15:44:18 +0530

    by xyz

--- GitDemo1/src/Demo.java
+++ GitDemo1/src/Demo.java
@@ -7,6 +7,9 @@ public class Demo {
 	public void m1() {
 		System.out.println("IN m1()");
 	}
+	public void m2() {
+		System.out.println("IN m2()");
+	}
 
 	public static void main(String[] args) {
 		System.out.println("Welcome to Git");
